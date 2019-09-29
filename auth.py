7f380028ff1bf1802811152865092ebedc06873d
fox <foxastronaut97@gmail.com>
2019-09-30 00:58:10 +0100

    moved join to auth

--- auth.py
+++ auth.py
@@ -1,11 +1,11 @@
 import sys
 import spotipy.util as util
 
-def retrieve_or_request_token(username, scope):
-  token = util.prompt_for_user_token(username, scope)
+def retrieve_or_request_token(username, scopes):
+  token = util.prompt_for_user_token(username, ' '.join(scopes))
 
   if token:
-    print("Token retrieved for", username)
+    print("Token retrieved for", username, "for the scopes of", ','.join(scopes))
     return token
   else:
     print("Error: Can't get token for", username)
